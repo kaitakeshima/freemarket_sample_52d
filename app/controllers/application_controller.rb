90ac5fd14d4dd2e58cba51d73e68d15893c05bb5
Keisuke-Takagi <k19990410@gmail.com>
2019-06-14 10:11:45 +0900

    devise_parameter_sanitizerでストロングパラメーター追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:buyer_number, :nickname, :family_name,:first_name, :family_name_kana, :first_name_kana, :birthday, :phone_number,:prefecture, :city, :address, :building ])
 end
