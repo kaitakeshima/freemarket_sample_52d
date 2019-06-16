848d53d668cf432f7e05f98bc249f35570e096bc
Keisuke-Takagi <k19990410@gmail.com>
2019-06-16 15:18:36 +0900

    devise scopeを使ったルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {
+    registrations: "users/registrations"
+  }
+  devise_scope :user do
+    get "/users/sign_up" => "registrations#new"
+    post "users/sign_up/user-info" => 'users/registrations#user-info'
+    post "users/sign_up/phone_number" => "users/registrations#phone_mumber"
+    post "users/sign_up/address" => "users/registrations#address"
+    post "users/sign_up/credit" => "users/registrations#credit"
+    post "users/sign_up/confirm" => "users/registrations#confirm"
+
+  end
+   root 'items#index'
 end
