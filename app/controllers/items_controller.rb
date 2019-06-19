22340341cf60e2f32f5444aae010b1d0a8af61fe
Keisuke-Takagi <k19990410@gmail.com>
2019-06-19 20:02:57 +0900

    ページ遷移完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+  def detail
+  end
+  def buy_confirmation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   #   get 'users/sign_up/phone' => 'users/registrations#'
   #   #deviseで追加するルーティング記載
   # end
-  resources :items
-  resources :users, only: [:show, :destroy] 
+  resources :items, only: [:new]
+  resources :users, only: [:destroy] 
   #その他追加するルーティングはこの下に記載
   get 'users/sign_up/new' => 'users#sign_up1' 
   get 'users/sign_up/member' => 'users#sign_up2'
@@ -16,7 +16,11 @@ Rails.application.routes.draw do
   get 'users/sign_up/card' => 'users#sign_up5'
   get 'users/sign_up/done' => 'users#sign_up6'
   get 'users/log_in/show' => 'users#log_in_view'
-  get 'users/mypage' => 'users#mypage_top'
-  get 'users/mypage' => 'users#mypage_top'
-  get 'users/mypage' => 'users#mypage_top'
+  get 'users/mypage' => 'users#mypage'
+  get 'users/mypage/destroy' => 'users#mypage_session_destroy'
+  get 'users/mypage/profile' => 'users#profile'
+  get 'users/mypage/credit' => 'users#credit'
+  get 'users/mypage/identification' => 'users#mypage_identification'
+  get 'items/show' => 'items#show'
+  get 'items/buy_confirmation' => 'items#buy_confirmation'
 end
