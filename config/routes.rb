fcbc71b8cb767c1caacf99c8b5e4de0a2b174d1f
Keisuke-Takagi <k19990410@gmail.com>
2019-06-19 13:15:14 +0900

    仮のルーティングとコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   devise_scope :user do
+    post 'users/sign_up/sns' => 'users/registrations'
     #deviseで追加するルーティング記載
   end
   resources :items
   resources :users, only: [:show, :destroy] 
   #その他追加するルーティングはこの下に記載
+  get 'users/view/sign_up' => 'users#user_sign_up'
 end
