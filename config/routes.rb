be1a1889406689a71aa7b3132e703f92cfc3fbe6
Keisuke-Takagi <k19990410@gmail.com>
2019-06-19 14:21:51 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   resources :items
   resources :users, only: [:show, :destroy] 
   #その他追加するルーティングはこの下に記載
-  get 'users/sign_up' => 'users#user_sign_up'
-  get 'users/sign_up/' => 'users#aaa_sign_up'
-  get 'users/sign_up/'
+  get 'users/sign_up' => 'users#sign_up1'
+  get 'users/sign_up' => 'users#sign_up2'
+  get 'users/sign_up' => 'users#sign_up3'
+  get 'users/sign_up'
 end
