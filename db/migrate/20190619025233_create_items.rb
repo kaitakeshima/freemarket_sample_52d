f8c04fbe13bceba4f09a63908300dc8f7889f565
Keisuke-Takagi <k19990410@gmail.com>
2019-06-19 12:10:29 +0900

    item のmigrationfile作成

--- db/migrate/20190619025233_create_items.rb
+++ db/migrate/20190619025233_create_items.rb
@@ -1,7 +1,19 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name, null:false
+      t.text :discription, null:false
+      t.integer :condition, null:false
+      t.integer :delivery_fee, null:false
+      t.integer :delivery_method, null:false
+      t.integer :delivery_days, null:false
+      t.integer :price, null:false
+      t.integer :size
+      t.references :category, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :brand, foreign_key: true
+      t.integer :buyer_number
+      t.integer :prefecture, null:false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_025134) do
+ActiveRecord::Schema.define(version: 2019_06_19_025233) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,27 @@ ActiveRecord::Schema.define(version: 2019_06_19_025134) do
     t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "discription", null: false
+    t.integer "condition", null: false
+    t.integer "delivery_fee", null: false
+    t.integer "delivery_method", null: false
+    t.integer "delivery_days", null: false
+    t.integer "price", null: false
+    t.integer "size"
+    t.bigint "category_id"
+    t.bigint "user_id", null: false
+    t.bigint "brand_id"
+    t.integer "buyer_number"
+    t.integer "prefecture", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_items_on_brand_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "buyer_number", null: false
     t.string "nickname", null: false
@@ -74,4 +95,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_025134) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "brands"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
