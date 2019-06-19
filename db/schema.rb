73bdfa038c4dbead309f88f936eccc3debc80246
Keisuke-Takagi <k19990410@gmail.com>
2019-06-19 11:53:25 +0900

    brandmigrationfile作成と、itemモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_023957) do
+ActiveRecord::Schema.define(version: 2019_06_19_025134) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
