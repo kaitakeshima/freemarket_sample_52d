81538ae019fd749043b06cbf06677005e36d48a8
Keisuke-Takagi <k19990410@gmail.com>
2019-06-19 14:53:54 +0900

    新規登録viewファイルの設置

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :destroy] 
   #その他追加するルーティングはこの下に記載
   get 'users/sign_up' => 'users#sign_up1'
-  get 'users/sign_up' => 'users#sign_up2'
-  get 'users/sign_up' => 'users#sign_up3'
-  get 'users/sign_up'
+  get 'users/sign_up/member' => 'users#sign_up2'
+  get 'users/sign_up/phone' => 'users#sign_up3'
+  get 'users/sign_up/address' => 'users#sign_up4'
+  get 'users/sign_up/card' => 'users#sign_up5'
+  get 'users/sign_up/done' => 'users#sign_up6'
+  get 'users/log_in/show' => 'user#log_in_view'
 end
