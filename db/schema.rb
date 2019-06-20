96b38b5d3b106469546b7e34ef308c8edb595286
Keisuke-Takagi <k19990410@gmail.com>
2019-06-20 13:31:37 +0900

    scheema commit01

--- db/schema.rb
+++ db/schema.rb
@@ -25,31 +25,6 @@ ActiveRecord::Schema.define(version: 2019_06_19_025233) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "installs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "buyer_number", null: false
-    t.string "nickname", null: false
-    t.string "image"
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.integer "birthday", null: false
-    t.integer "phone_number", null: false
-    t.integer "prefecture", null: false
-    t.string "city"
-    t.string "address"
-    t.string "building"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_installs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "discription", null: false
@@ -73,6 +48,18 @@ ActiveRecord::Schema.define(version: 2019_06_19_025233) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "buyer_number"
+    t.string "nickname", null: false
+    t.string "image"
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.integer "birthday", null: false
+    t.integer "phone_number", null: false
+    t.integer "prefecture", null: false
+    t.string "city"
+    t.string "address"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
