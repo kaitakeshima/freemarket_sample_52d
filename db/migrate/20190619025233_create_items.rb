e80140314a60132b0e3ddcb6cddd38c5df47bfb1
Keisuke-Takagi <k19990410@gmail.com>
2019-06-22 12:26:03 +0900

    categoryカラムの削除とbrandカラムの作成

--- db/migrate/20190619025233_create_items.rb
+++ db/migrate/20190619025233_create_items.rb
@@ -1,20 +1,20 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      # t.string :name, null:false
-      # t.text :discription, null:false
-      # t.integer :condition, null:false
-      # t.integer :delivery_fee, null:false
-      # t.integer :delivery_method, null:false
-      # t.integer :delivery_days, null:false
-      # t.integer :price, null:false
-      # t.integer :size
-      # t.references :category, foreign_key: true
       # t.references :user, null: false, foreign_key: true
-      # t.references :brand, foreign_key: true
-      # t.integer :buyer_number
-      # t.integer :prefecture, null:false
-      # t.string :image, null:false
+      t.string :name, null:false
+      t.text :discription, null:false
+      t.integer :condition, null:false
+      t.integer :delivery_fee, null:false
+      t.integer :delivery_method, null:false
+      t.integer :delivery_days, null:false
+      t.integer :price, null:false
+      t.integer :size
+      t.references :user, null: false, foreign_key: true
+      t.string :brand 
+      t.integer :buyer_number
+      t.integer :prefecture, null:false
+      t.string :image, null:false
       t.timestamps
     end
   end
