d9a94425ce93fbdf4245a9b4d59b43e216125117
dai0204 <nandai0204@gmail.com>
2019-06-23 20:14:22 +0900

    SNS認証

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,4 +62,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def create
+    #binding.pry
+     if params[:user][:password] == "" #sns登録なら
+       params[:user][:password] = "Devise.friendly_token.first(6)" #deviseのパスワード自動生成機能を使用
+       params[:user][:password_confirmation] = "Devise.friendly_token.first(6)"
+       super
+       # binding.pry
+       sns = SnsCredential.update(user_id:  @user.id)
+     else #email登録なら
+       # binding.pry
+       super
+     end
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_many :items
   validates :nickname,  presence: true
   validates :family_name, presence: true
@@ -11,4 +11,41 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :city, presence: true
   validates :birthday, presence: true
+
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+
+    if snscredential.present?
+      user = User.where(id: snscredential.user_id).first
+      unless user.present?
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+      end
+      sns = snscredential
+
+    else
+      user = User.where(email: auth.info.email).first
+      if user.present?
+        sns = SnsCredential.new(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+        )
+      else
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email,
+        )
+        sns = SnsCredential.create(
+          uid: uid,
+          provider: provider
+        )
+      end
+    end
+    return { user: user, sns_id: sns.id }
+  end
 end
