8aeeed1ba5888f6fb3f64ff85bdb7b3624294f66
Keisuke-Takagi <k19990410@gmail.com>
2019-06-23 16:25:04 +0900

    使わなかったjsファイルの削除

--- app/assets/javascripts/sign_up2.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  // e.preventDefault();
-  $(".member-main__container__head").on("click", function() {
-    var birthday = $(".select-wrap__default1 option:selected").text();
-    $("li").append(birthday);
-    debugger
-    console.log("クリックされました")
-  })
-})
