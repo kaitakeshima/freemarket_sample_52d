12aaeb815b3abb113b7f15f6ba13cb8cfbf1e916
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-06-24 15:04:45 +0900

    [WIP]不要箇所コメントアウト
    # WHAT
    モデルファイル内、アソシエーションに関する記述をコメントアウト。
    
    # WHY
    現在必要ないため

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
-has_many :images
-belongs_to :brand
-belongs_to :category
-belongs_to :user
+  mount_uploader :image, ImageUploader
+  # has_many :images
+  # belongs_to :brand
+  # belongs_to :category
+  belongs_to :user
 end
