4d33f3a087ecbe94983d019033ac8dc18f25cc04
dai0204 <nandai0204@gmail.com>
2019-06-25 18:26:09 +0900

    商品詳細ページのサーバーサイド

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,8 @@ class ItemsController < ApplicationController
   
   def show
     @item = Item.find(params[:id])
-
+    @items = Item.all.order('id DESC').limit(6)
+    binding.pry
   end
 
   def detail
