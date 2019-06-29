ad58c0f709576c448a3d825f2f514a77ee3e0b34
Keisuke-Takagi <k19990410@gmail.com>
2019-06-29 16:32:26 +0900

    ローカル環境本番環境でログイン後Redirect先がおかしくなる問題の解消

--- app/assets/javascripts/error.js
+++ app/assets/javascripts/error.js
@@ -4,11 +4,41 @@ $(function(){
   var $dir1 = location.href.split("/")
   var $dir2 = $dir1[$dir1.length -1];
   var url = $dir2
-  $(window).on('load', function(e){
+  $(document).ready(function(){
     if ("flash_error" == url){
     alert("エラーが発生しました。")
-    }
-  })
+    }else{}
+    if (location.href == "http://localhost:3000/" && (document.referrer == "" || document.referrer == "http://localhost:3000/") ){
+      var reload_index = $(function() {
+        if (sessionStorage.getItem("counter") == null){
+          sessionStorage.setItem("counter", 0);
+        }else{}
+        var counter = sessionStorage.getItem("counter")
+        if (counter == 0){
+          sessionStorage.setItem("counter", 1);
+          setInterval(location.reload(), 1);
+        }else{sessionStorage.setItem("counter", 0)
+      }
+      });
+      reload_index
+    }else{}
+    if(location.href == "http://3.113.81.197/" && (document.referrer == "" || document.referrer == "http://3.113.81.197/" )){
+      var reload_index = $(function() {
+        if (sessionStorage.getItem("counter") == null){
+          sessionStorage.setItem("counter", 0);
+        }else{}
+        var counter = sessionStorage.getItem("counter")
+        if (counter == 0){
+          sessionStorage.setItem("counter", 1);
+          setInterval(location.reload(), 1);
+        }else{sessionStorage.setItem("counter", 0)
+  
+      }
+      });
+      reload_index
+    }else{}
+  });
+
   $(".btn-square-pop-half-delete").on('click', function(){
     if(window.confirm("データを削除しますがよろしいですか？")) {
         location.href = $(this).attr('href');
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,34 @@ class UsersController < ApplicationController
     session[:aa] = 1
   end
   def show
-    if session[:aa] == 0
-      @address = House.find_by(user_id: current_user.id)
-      session[:aa] = 6
+    if user_signed_in?
+      if current_user.id == params[:id].to_i
+          @address = House.find_by(user_id: current_user.id)
+          session[:aa] = 6
+      else
+        redirect_back(fallback_location: root_path)
+        flash[:alert] = "予期しないアクセスが発生しました"
+      end
     else
       redirect_back(fallback_location: root_path)
       flash[:alert] = "予期しないアクセスが発生しました"
+    end
   end
   def mypage_identification
-    if session[:aa] = 6
-      @address = House.find_by(user_id: current_user.id)
-      binding.pry
-      if @address == nil
-        session[:aa] = 7
-        @address = House.new(user_id: current_user.id)
+    if user_signed_in?
+      if current_user.id == params[:id].to_i
+        @address = House.find_by(user_id: current_user.id)
+        binding.pry
+        if @address == nil
+          session[:aa] = 7
+          @address = House.new(user_id: current_user.id)
+        else
+          session[:aa] = 8
+          redirect_to mypage_identification_update_user_path(current_user)
+        end
       else
-        session[:aa] = 8
-        redirect_to mypage_identification_update_user_path(current_user)
+        redirect_back(fallback_location: root_path)
+        flash[:alert] = "予期しないアクセスが発生しました"
       end
     else
       redirect_back(fallback_location: root_path)
@@ -28,9 +39,19 @@ class UsersController < ApplicationController
     end
   end
   def mypage_identification_update
-    if session[:aa] == 8
-    @address =  House.find_by(user_id: current_user.id)
-    binding.pry
+    if user_signed_in?
+      if current_user.id == params[:id].to_i
+        if session[:aa] == 8
+        @address =  House.find_by(user_id: current_user.id)
+        binding.pry
+        else
+          redirect_back(fallback_location: root_path)
+          flash[:alert] = "予期しないアクセスが発生しました"
+        end
+      else
+        redirect_back(fallback_location: root_path)
+        flash[:alert] = "予期しないアクセスが発生しました"
+      end
     else
       redirect_back(fallback_location: root_path)
       flash[:alert] = "予期しないアクセスが発生しました"
