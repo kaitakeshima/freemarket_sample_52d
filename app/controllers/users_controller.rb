dfe2ed6fb10ae2b5d6ad67c2c7b17cc69526c376
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-06-30 13:06:21 +0900

    [WIP]出品アイテム情報取得
    # WHAT
    現在ユーザーの出品アイテム情報を取得する
    
    # WHY
    一覧表示するため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def sign_up_first
     session[:aa] = 1
   end
+  
+  def mypage_syuppin
+    @items = Item.where(user_id: current_user.id)
+  end
 end
